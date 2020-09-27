e0e6e78f30aa21f86ba196b03b7a4b54157e4a9e
mutwiri-2 <devmutwiri2@gmail.com>
2020-09-27 19:10:16 +0300

    import needed libraries to compose email

--- mailing_client/mailing_client.py
+++ mailing_client/mailing_client.py
@@ -2,6 +2,12 @@ import smtplib
 import os
 from pathlib import Path
 
+from email import encoders
+from email.mime.text import MIMEText # for text in the mail
+from email.mime.base import MIMEBase # for attachments
+from email.mime.multipart import MIMEMultipart # for the whole thing
+
+
 # define a server
 server = smtplib.SMTP(host='smtp.gmail.com', port=25)
 
